a4ce355dc4193a69eb5f57b317d673e3953d7415
dhkim9179 <dhkim9179@gmail.com>
2021-06-01 13:29:23 +0900

    .

---
+++ Vue/src/test/java/com/xcure/vue/VueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcure.vue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
